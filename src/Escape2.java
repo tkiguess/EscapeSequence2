990c3cd336bafb19d995e64e4f8f5c601c52681e
Tafkir Ahmed <ahmedtafkir@gmail.com>
2018-07-11 17:16:40 -0400

    commitments

--- src/Escape2.java
+++ src/Escape2.java
@@ -9,8 +9,8 @@ public class Escape2 {
 
     public static void main( String[] args ) {
 
-        System.out.println("$1,000.00   4       $4,000.00");
-        System.out.println("$   50.00   8         $400.00");
+        System.out.println("$1,000.00   4 \t    $4,000.00");
+        System.out.println("$   50.00   8 \t    $400.00");
         System.out.println("  ");
         System.out.println("______________________________");
         System.out.println("TOTAL:      12      $4,400.00");
