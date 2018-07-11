12b1a66baef6e3a6a963fcb5927f240958f2cf3c
Tafkir Ahmed <ahmedtafkir@gmail.com>
2018-07-11 17:05:45 -0400

    commitments

---
+++ src/Escape2.java
@@ -0,0 +1,20 @@
+/*Cost      Quantity   Total
+        $1,000.00   4       $4,000.00
+        $   50.00   8         $400.00
+        ______________________________
+        TOTAL:      12      $4,400.00
+        */
+
+public class Escape2 {
+
+    public static void main( String[] args ) {
+
+        System.out.println("$1,000.00   4       $4,000.00");
+        System.out.println("$   50.00   8         $400.00");
+        System.out.println("  ");
+        System.out.println("______________________________");
+        System.out.println("TOTAL:      12      $4,400.00");
+
+    }
+
+}
